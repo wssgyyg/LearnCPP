36e89a78ac727c3b5672a0e136355f5e0c487bfc
wssgyyg <827580591@qq.com>
2018-10-24 22:00:56 +0800

    Initial Commit

---
+++ LearnCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnCPP
+//
+//  Created by 杨永贵 on 24/10/18.
+//  Copyright © 2018年 Tencent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
